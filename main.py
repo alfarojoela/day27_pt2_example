188d6b3b125fba484cc173444cd738bb6bf7e59b
Joel Alfaro <alfarojoela@outlook.com>
2021-10-06 19:19:40 -0700

    grid use.  more comments.

--- main.py
+++ main.py
@@ -9,18 +9,36 @@ window = Tk()
 window.title("My First GUI Program")
 
 window.minsize(width=500, height = 300)
+window.config(padx=20, pady=20)
 
 #Label
 
 my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
 my_label.config(text="New Text")
-my_label.pack()
+my_label.grid(row=0, column=0)
+my_label.config(padx=50, pady=50)
 
 #Button
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+#button.pack()  #if pack method not used button will not appear in window when program is run
+button.grid(row=1, column=1)
 
 #Entry
 input = Entry(width=10)
 print(input.get())
-input.pack()
+input.grid(row=2, column=3)
+
+#New Button
+new_button = Button(text="New Button to Click", command=button_clicked)
+new_button.grid(row=0, column=2)
+
+#pack places widgets.  places at top and then adds entries below.  lacks precision
+#place has better precision uses x and y coordinates.
+#grid layout is columns and rows.  relative to other components.
+
+
+
+
+
+
+window.mainloop()
